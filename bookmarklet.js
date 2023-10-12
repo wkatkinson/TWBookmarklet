346dc3d16cccaf54b16197a0bef192d36c03f335
Kellet <42361411+wkatkinson@users.noreply.github.com>
2023-10-12 12:34:14 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,16 +4,17 @@ javascript:(function() {
     let triplePixelShopUrl = typeof window.TriplePixelData !== 'undefined' && window.TriplePixelData.TripleName;
     let triplePixelVersion = typeof window.TriplePixelData !== 'undefined' && window.TriplePixelData.ver;
     let shopifyDetected = typeof window.Shopify !== 'undefined';
+    let triplePixelState = typeof window.TriplePixel !== 'undefined' && window.TriplePixel('state');
     let shopifyShop = typeof window.Shopify !== 'undefined' && window.Shopify.shop;
-    let ShopifyTheme = typeof window.Shopify !== 'undefined' && window.Shopify.theme.name;
-    let requestDetected = false;
-    let requestSuccess = false;
+    let ShopifyTheme = typeof window.Shopify !== 'undefined' && window.Shopify.theme && window.Shopify.theme.name;
+    let p1requestDetected = false;
+    let p1requestSuccess = false;
+    
 
-    if (triplePixelVersion === "2.0") {
-        var pixel2state = TriplePixel('state');
-    }
     let tripleHeadlessDetected = typeof window.TripleHeadless !== 'undefined';
     let triplePixelEndpoint= "https://open.pixel.api.whale3.io/trek/add";
+    let triplePixelV2eventEndpoint = "https://api.config-security.com/event";
+    let triplePixelV2monkeyIdEndpoint = "https://api.config-security.com/model";
     var message = '';
 
     if (window.performance && window.performance.getEntriesByType) {
@@ -21,9 +22,19 @@ javascript:(function() {
         if (requests.length > 0) {
             for (var i = 0; i < requests.length; i++) {
                 if (requests[i].name.includes(triplePixelEndpoint)) {
-                    requestDetected = true;
+                    p1requestDetected = true;
                     if (requests[i].responseStatus && requests[i].responseStatus === 200) {
-                        requestSuccess = true;
+                        p1requestSuccess = true;
+                    }
+                } else if (requests[i].name.includes(triplePixelV2eventEndpoint)) {
+                    p2EventrequestDetected = true;
+                    if (requests[i].responseStatus && requests[i].responseStatus === 200) {
+                        p2EventrequestSuccess = true;
+                    }
+                } else if (requests[i].name.includes(triplePixelV2monkeyIdEndpoint)) {
+                    p2MonkeyIdrequestDetected = true;
+                    if (requests[i].responseStatus && requests[i].responseStatus === 200) {
+                        p2MonkeyIdrequestSuccess = true;
                     }
                 }
             }
@@ -40,6 +51,13 @@ javascript:(function() {
             message+="\nPixel Snippet Installation: Triple Pixel is detected.";
             message+="\nPixel Version: " + triplePixelVersion;
             message+="\nPixel Shop URL: " + triplePixelShopUrl; 
+            message+="\n\n[ Pixel Snippet Installation GOOD ]";
+        } else if (!triplePixelData && triplePixelState) {
+            message = "=== SHOP INFO ===";
+            message+="\nShopify Installation: Page is hosted by Shopify.";
+            message+="\n\n=== PIXEL INSTALLATION ===";
+            message+="\nPixel Snippet Installation: Triple Pixel is detected.";
+            message+="\nPixel Version: v2 Thank You Snippet";
             message+="\n\n[ Pixel Snippet Installation GOOD ]";
         } else {
             message = "=== SHOP INFO ===";
@@ -59,8 +77,17 @@ javascript:(function() {
             message+="\nPixel Snippet Installation: Triple Pixel is detected.";
             message+="\nPixel Version: " + triplePixelVersion;
             message+="\nHeadless Snippet: Detected.";
-            message+="\nPixel Shop URL: " + window.TripleHeadless;
+            message+="\nPixel Shop URL (TripleHeadless): " + window.TripleHeadless;
             message+="\n\n[ Pixel Snippet Installation GOOD ]";
+        } else if (!triplePixelData && triplePixelState) {
+                message = "=== SHOP INFO ===";
+                message+="\nShopify Installation: Headless, Third-Party, or Non-Shopify Page.";
+                message+="\n\n=== PIXEL INSTALLATION ===";
+                message+="\nPixel Snippet Installation: Triple Pixel is detected.";
+                message+="\nPixel Version: v2 Thank You Snippet";
+                message+="\nHeadless Snippet: Detected.";
+                message+="\nPixel Shop URL (TripleHeadless): " + window.TripleHeadless;
+                message+="\n\n[ Pixel Snippet Installation GOOD ]";
         } else {
             message = "=== SHOP INFO ===";
             message+="\nShopify Installation: Headless, Third-Party, or Non-Shopify Page.";
@@ -68,9 +95,10 @@ javascript:(function() {
             message+="\nPixel Snippet Installation: Triple Pixel is detected.";
             message+="\nPixel Version: Pre-v.1.8";
             message+="\nHeadless Snippet: Detected.";
+            message+="\nPixel Shop URL (TripleHeadless): " + window.TripleHeadless;
             message+="\n\n[ Pixel Snippet Installation GOOD ]";
         }
-    } else if (triplePixelDetected) {
+    } else if (triplePixelDetected && !shopifyDetected) {
         if (triplePixelData) {
             message = "=== SHOP INFO ===";
             message+="\nShopify Installation: Headless, Third-Party, or Non-Shopify Page.";
@@ -103,17 +131,29 @@ javascript:(function() {
         message+="\nPixel Snippet Installation: Triple Pixel is NOT detected!!";
         message+="\n\n[!! Pixel Snippet Installation BAD. Install Pixel Snippet. !!]";
     }
-
-    if (requestDetected && requestSuccess && triplePixelVersion !== "2.0") {
-        message += "\n\n=== PIXEL TRACKING STATUS ===\n\nPixel Request: Detected\nPixel Request Status: 200\n\n[ Pixel is SUCCESSFULLY sending data ]";
-    } else if (requestDetected && !requestSuccess && requests.length > 0 && !requestSuccess && !triplePixelVersion || triplePixelVersion !== "2.0") {
-        message += "\n\n=== PIXEL TRACKING STATUS ===\n[[ NOTE: Be Sure to wait for the entire page to finish loading before checking tracking status. ]]\n\nPixel Request: Detected\nPixel Request Status: Failed\n\n[!! Pixel is NOT successfully sending data. Check server status. !!]";
-    } else if (!requestDetected && !triplePixelVersion || triplePixelVersion !== "2.0") {
-        message += "\n\n=== PIXEL TRACKING STATUS ===\n[[ NOTE: Be Sure to wait for the entire page to finish loading before checking tracking status. ]]\n\nPixel Request: NOT DETECTED\n[!! Pixel is NOT successfully sending data. Request is being blocked. !!]";
-    } else if (triplePixelVersion === "2.0" && pixel2state === "Ready") {
-        message += "\n\n=== PIXEL TRACKING STATUS ===\nPixel Status: Ready\n[ Pixel is SUCCESSFULLY sending data ]";
-    } else if (triplePixelVersion === "2.0" && pixel2state === "Installed") {
-        message += "\n\n=== PIXEL TRACKING STATUS ===\nPixel Status: Installed, but not 'ready'\n[!! Pixel is installed, but is not sending data. Review your installation. !!]";
+    if (triplePixelState) {
+        message += "\n\n=== PIXEL TRACKING STATUS ===\nPixel Status: " + triplePixelState;
+        if (triplePixelV2monkeyIdEndpoint) {
+            message+="\nPixel ID Request:\n("+triplePixelV2monkeyIdEndpoint+")\nDetected";
+        } else {
+            message+="\nPixel ID Request:\n("+triplePixelV2monkeyIdEndpoint+")\nNOT DETECTED";
+        }
+        if (triplePixelV2eventEndpoint) {
+            message+="\n\nPixel Event Request:\n("+triplePixelV2eventEndpoint+")\nDetected";
+        } else {
+            message+="\n\nPixel Event Request:\n("+triplePixelV2eventEndpoint+")\nNOT DETECTED";
+        }
+        if (triplePixelV2eventEndpoint && triplePixelV2monkeyIdEndpoint) {
+            message += "\n\n[ Pixel is SUCCESSFULLY sending data ]";
+        } else {
+            message += "\n\n[!! Pixel is installed but is NOT successfully sending data. Make sure the page has finished loading. If so, review your installation !!]";
+        }
+    } else if (p1requestDetected && p1requestSuccess) {
+        message += "\n\n=== PIXEL TRACKING STATUS ===\n\nPixel Request:\n("+triplePixelEndpoint+")\nDetected\n\nPixel Request Status: 200\n\n[ Pixel is SUCCESSFULLY sending data ]";
+    } else if (p1requestDetected && !p1requestSuccess && requests.length > 0) {
+        message += "\n\n=== PIXEL TRACKING STATUS ===\n[[ NOTE: Be Sure to wait for the entire page to finish loading before checking tracking status. ]]\n\nPixel Request ("+triplePixelEndpoint+"): Detected\nPixel Request Status: Failed\n\n[!! Pixel is NOT successfully sending data. Check server status. !!]";
+    } else if (!p1requestDetected) {
+        message += "\n\n=== PIXEL TRACKING STATUS ===\n[[ NOTE: Be Sure to wait for the entire page to finish loading before checking tracking status. ]]\n\nPixel Request ("+triplePixelEndpoint+"): NOT DETECTED\n[!! Pixel is NOT successfully sending data. Request is being blocked. !!]";
     }
     console.log(message);
     alert(message);
